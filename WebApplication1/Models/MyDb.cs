ad5ac223b2e81b95bd94387f48debb8e24e13e80
Administrator <Administrator@6-206-A55>
2019-05-16 09:54:13 +0800

    asd ads

---
+++ WebApplication1/Models/MyDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<WebApplication1.Models.Xueshengxinxi> Xueshengxinxis { get; set; }
+
+        public DbSet<WebApplication1.Models.Chengji> Chengjis { get; set; }
+
+       // public System.Data.Entity.DbSet<WebApplication1.Models.Chengji> Chengjis { get; set; }
+    }
+}
---
+++ WebApplication1/Models/MyDbInit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MyDbInit
+        : System.Data.Entity.CreateDatabaseIfNotExists<MyDb>
+    {
+        protected override void Seed(MyDb context)
+        {
+            base.Seed(context);
+
+
+            context.Xueshengxinxis.Add(new Xueshengxinxi
+            {
+                Xueshengxuehao = "20180001",
+                Xueshengxingming = "张三",
+                Xueshengxingbie = "男"
+
+            });
+            context.Xueshengxinxis.Add(new Xueshengxinxi
+            {
+                Xueshengxuehao = "20180002",
+                Xueshengxingming = "李四",
+                Xueshengxingbie = "女"
+            });
+            context.Chengjis.Add(new Chengji
+            {
+                Xuhao="s001",
+                Xueshengxuehao = "20180002",
+                Xueshengxingming = "李四",
+                Xueshengxingbie = "女",
+                Kechengmingcheng="体育",
+                Kechengchengji=98
+            });
+
+
+        }
+
+
+    }
+}
